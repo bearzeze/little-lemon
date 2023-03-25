4af7fd3f3fc817a7d3d91ee78f7598cb0dc4b859
Zejd Imamovic <i.zejd@hotmail.com>
2023-03-25 14:58:04 +0100

    Authorization finished

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from .models import Menu, Booking
 from .serializers import MenuSerializer, BookingSerializer
 from rest_framework import generics, viewsets
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 
 # Create your views here.
 def home(request):
@@ -20,4 +24,11 @@ class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
 
+
+@api_view()
+@permission_classes([IsAuthenticated])
+@authentication_classes([TokenAuthentication])
+def secret_message(request):
+    return JsonResponse({"secret_message": "Bebac 1987"})
